a70191350bacd8170f7309ffda0680a43cdafdb9
linttttt <91177710+linttttt@users.noreply.github.com>
2023-10-25 16:06:41 +0800

    update

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import {resolve} from 'path'
 
+// const root = resolve(__dirname, 'src')
+const outDir = resolve(__dirname, 'docs')
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  base: '/112-1-web-app-my/',
+  build: {
+    outDir,
+    emptyOutDir: true
+}})
